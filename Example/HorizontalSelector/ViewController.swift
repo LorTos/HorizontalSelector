d70d867f32730eb3e32569f2a12a80637a06655b
LorTos <lorenzotoscanidc@gmail.com>
2020-07-22 18:23:08 +0200

    public properties and started example project

--- Example/HorizontalSelector/ViewController.swift
+++ Example/HorizontalSelector/ViewController.swift
@@ -7,18 +7,38 @@
 //
 
 import UIKit
+import HorizontalSelector
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+	
+	private lazy var expandedSelector: HorizontalSelectorView = {
+		let selector = HorizontalSelectorView(with: ["option 1", "option 2", "option 3", "option 4"], initialSelection: 0)
+		selector.backgroundColor = .black
+		selector.tintColor = .white
+		return selector
+	}()
+	
+	private lazy var collapsingSelector: HorizontalSelectorView = {
+		let selector = HorizontalSelectorView(with: ["option 1", "option2", "option 3"])
+		selector.backgroundColor = .black
+		selector.tintColor = .white
+		selector.isCollapsible = true
+		selector.actionString = "Select option"
+		if #available(iOS 13.0, *) {
+			selector.actionImage = UIImage(systemName: "arrowtriangle.right.fill", compatibleWith: nil)
+		}
+		return selector
+	}()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		view.addSubview(expandedSelector)
+		expandedSelector.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+		
+		view.addSubview(collapsingSelector)
+		collapsingSelector.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+	}
+	
 }
 
--- Example/Pods/Target Support Files/Pods-HorizontalSelector_Example/Pods-HorizontalSelector_Example-acknowledgements.markdown
+++ Example/Pods/Target Support Files/Pods-HorizontalSelector_Example/Pods-HorizontalSelector_Example-acknowledgements.markdown
@@ -5,6 +5,10 @@ This application makes use of the following third party libraries:
 
 Copyright (c) 2020 LorTos <lorenzotoscanidc@gmail.com>
 
+MIT License
+
+Copyright (c) 2020 Lorenzo Toscani De Col
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -20,6 +24,7 @@ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
